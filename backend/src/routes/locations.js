d2fdaa5054f69435e5ab5351fbd3a5db38959172
lago-pete <lagonegropeter@gmail.com>
2026-03-11 15:47:22 -0700

    feat: add POST /locations route with database cache check

--- backend/src/routes/locations.js
+++ backend/src/routes/locations.js
@@ -1,21 +1,47 @@
-const express = require('express')
-const router = express.Router()
-const {searchLocations} = require('../services/placesService')
+const express = require("express");
+const router = express.Router();
+const { searchLocations } = require("../services/placesService");
+const pool = require("../db/pool");
 
-
-router.get('/search', async (req,res) => {
-    try{    
-        const query = req.query.q
-        if(!query){
-            return res.status(400).json({error:'Search query is required'})
-        }
-        const results = await searchLocations(query)
-        res.json(results)
-    }catch(error){
-        console.error('The error is:', error)
-        res.status(500).json({error:'Failed to search DB'})
+router.get("/search", async (req, res) => {
+  try {
+    const query = req.query.q;
+    if (!query) {
+      return res.status(400).json({ error: "Search query is required" });
     }
-})
+    const results = await searchLocations(query);
+    res.json(results);
+  } catch (error) {
+    console.error("The error is:", error);
+    res.status(500).json({ error: "Failed to search DB" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const address = req.body.address;
+    const google_place_id = req.body.google_place_id;
+    const name = req.body.name;
+
+    const result = await pool.query("SELECT * FROM locations WHERE google_place_id = $1",[google_place_id])
+
+      
+    if (result.rows.length > 0) {
+      return res.status(200).json(result.rows[0]);
+    }
 
 
-module.exports = router
+    const newLocation = await pool.query(
+      "INSERT INTO locations (address, google_place_id, name) VALUES ($1, $2, $3) RETURNING *",
+      [address, google_place_id, name],
+    );
+
+    return res.status(201).json(newLocation.rows[0]);
+
+  } catch(error) {
+    console.error('Error is: ', error)
+    return res.status(500).json({error: "failed to load location."})
+  }
+});
+
+module.exports = router;
